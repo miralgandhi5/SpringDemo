1bf56613b54bdc87f0ac68f93edbeaee2c366f50
miralgandhi <miralgandhi5@gmail.com>
2017-07-06 15:41:11 +0530

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("main/resources/spring-config.xml");
-        Database database = (Database) applicationContext.getBean("db");
-        System.out.println(database);
-        Restaurant teaRestaurant = (Restaurant) applicationContext.getBean("teaRestaurant");
-        Restaurant expressTeaRestaurant = (Restaurant) applicationContext.getBean("expressTeaRestaurant");
-        teaRestaurant.getHotDrink().prepareHotDrink();
-        expressTeaRestaurant.getHotDrink().prepareHotDrink();
-        Complex complex=(Complex)applicationContext.getBean("complex");
-        System.out.println(complex);
+
+         Restaurant restaurant=(Restaurant)applicationContext.getBean("restaurant");
+         restaurant.getHotDrink().prepareHotDrink();
 
 
     }
